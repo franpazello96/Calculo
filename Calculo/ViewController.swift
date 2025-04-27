6a60a22d68db5d1f1d570db7b9990716b89d9f4e
Francielly Pazello <74922124+franpazello96@users.noreply.github.com>
2025-04-27 17:22:04 -0300

    Initial Commit

---
+++ Calculo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculo
+//
+//  Created by francielly pazello on 27/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
